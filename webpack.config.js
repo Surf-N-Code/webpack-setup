404a25726c23c5eba146347b8c2b9a895b4043af
Norman Dilthey <norman.dilthey@schuettflix.com>
2022-11-21 20:11:15 +0100

    chore: add dev server config & cleanup dist folder after every build

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,18 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "[name]-[contenthash].js",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: false,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
