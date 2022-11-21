247d898a47c30d1b487dfa7a17c13d3e8eeb6ec2
Norman Dilthey <norman.dilthey@schuettflix.com>
2022-11-21 20:40:45 +0100

    chore: add webpack loader for images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import { getJoke } from './joke';
 import './styles/main.scss';
+import './assets/laughing.svg';
+import laughing from './assets/laughing.svg';
 const text = document.createElement('span');
 text.innerText = getJoke();
 text.classList = 'text-red';
 document.body.appendChild(text);
+
+const laughImg = document.createElement('img');
+laughImg.src = laughing
+
+document.body.appendChild(laughImg);
